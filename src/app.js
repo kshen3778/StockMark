4793d898ec2a25465201a33c07a3a7d1004aaa79
kshen3778 <kshen3778@gmail.com>
2015-07-01 20:43:09 -0400

    Expanded Configuration
    Users can now add up to 20 companies and it will display on the menu.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 /**
  * StockMark Market Watcher app
- Current bugs:
- Menu must contain 5 companies otherwise it won't work.
+ TODO: Shake to update
  */
 var Settings = require('settings');
 var UI = require('ui');
@@ -10,16 +9,17 @@ var ticker;
 var menuItems;
 //menu
 var resultsMenu;
+
 //show opening card
 var splashCard = new UI.Card({
   title: "Open App configuration",
-  body: "Click the gear icon on the phone beside the app and type in your company's stock ticker symbol."
+  body: "Click the gear icon for this app on the phone."
 });
 
 splashCard.show();
 
-
-var parseFeed = function(data,quantity){
+//retrieves data from json
+var parseFeed = function(data){
   var items = [];
   for (var key in data) {
     if (data.hasOwnProperty(key)) {
@@ -28,16 +28,12 @@ var parseFeed = function(data,quantity){
       if(title === ""){
         title = "Add another";
       }
-      console.log(title);
       //add to menu items array
       items.push({
         title: title
       });
     }
   }
-  //console.log(JSON.stringify(data));
-  
-  //return array
   return items;
 };
 
@@ -47,13 +43,10 @@ Settings.config(
     url: 'http://omicrontech.comuv.com/config-page.html' 
   },
   function(e) {
-    console.log('closed configurable');
-    // Show the parsed response
-  /*  ticker = JSON.stringify(e.options.ticker1);
-    ticker = ticker.replace(/"/g,""); 
-    var URL = "http://dev.markitondemand.com/Api/Quote/json?" + "symbol=" + ticker; */
-     menuItems = parseFeed(e.options,5); //load menu items 
-    resultsMenu = new UI.Menu({
+    console.log(Object.keys(e.options).length);
+    menuItems = parseFeed(e.options); //load menu items 
+    //create menu
+    resultsMenu = new UI.Menu({ //create menu
       sections: [{
         title: 'Companies',
         items: menuItems
@@ -66,41 +59,63 @@ Settings.config(
       ticker = e.item.title; //get clicked ticker
       ticker = ticker.replace(/"/g,""); //remove "" in string
       if(ticker != "Add another"){
-      var URL = "http://dev.markitondemand.com/Api/Quote/json?" + "symbol=" + ticker; 
-      //make ajax call to retrieve data
-      ajax(
-        {
-          url: URL,
-          type: 'json'
-        },
-        function(data) {
-          // Success!
-          console.log('Successfully fetched stock data!');
-          var name = data.Data.Name;
-          var symbol = data.Data.Symbol;
-          var lastprice = data.Data.LastPrice;
-          var openprice = data.Data.Open;
-          var change = data.Data.Change;
-          var percentchange = data.Data.ChangePercent;
-          //percentchange = percentchange.substring(0,2);
-          percentchange = +percentchange.toFixed(2);
-          change = +change.toFixed(2);
-          var quotecard = new UI.Card({
-            title: name + ", " + "(" + symbol + ")",
-            body: "Last Price: " + lastprice + "\n" + "Open Price: " + openprice + "\n" + "Change: " + change + " (" + percentchange + "%)"
-          });
-          quotecard.show();
-        },
-        function(error) {
-          // Failure!
-          console.log('Failed fetching stock data: ' + error);
-        }
-      );
+          var URL = "http://dev.markitondemand.com/Api/Quote/json?" + "symbol=" + ticker; 
+          //make ajax call to retrieve data
+          ajax(
+            {
+              url: URL,
+              type: 'json'
+            },
+            function(data) {
+              //if such ticker doesn't exist
+              if(data.hasOwnProperty("Message")){ 
+                var invalid = new UI.Card({
+                title: "Error loading data",
+                body: "No symbol matches found. One or more ticker symbols may be invalid."
+              });
+              invalid.show();
+              }
+              // retrieve data for clicked company
+              console.log('Successfully fetched stock data!');
+              //get JSON data key values
+              var name = data.Data.Name;
+              var symbol = data.Data.Symbol;
+              var lastprice = data.Data.LastPrice;
+              var openprice = data.Data.Open;
+              var change = data.Data.Change;
+              var percentchange = data.Data.ChangePercent;
+              percentchange = +percentchange.toFixed(2); //correct to 2 decimals
+              change = +change.toFixed(2); //correct to 2 decimals
+              //put + or - sign in front of change and percentchange
+              if(change >= 0.00 ){
+                change = "+" + change;
+              }
+              if(percentchange >= 0.00){
+                percentchange = "+" + percentchange;
+              }
+              //create the stock quote card
+              var quotecard = new UI.Card({
+                title: name + ", " + "(" + symbol + ")",
+                body: "Last Price: " + lastprice + "\n" + "Open Price: " + openprice + "\n" + "Change: " + change + " (" + percentchange + "%)"
+              });
+              quotecard.show();
+            },
+            function(error) {
+              // Failure!
+              console.log('Failed fetching stock data: ' + error);
+              var errorcard = new UI.Card({
+                title: "Error loading data",
+                body: "Ensure you have proper internet connection and try restarting the app."
+              });
+              errorcard.show();
+              
+            }
+          );
       }
-      else{
+      else{ //if user clicked on empty card
         var instructions = new UI.Card({
             title: "Add another",
-            body: "Open app configuration to set your companies(maximum 5)."
+            body: "Add another ticker via the configuration."
         });
 
         instructions.show();
@@ -108,14 +123,17 @@ Settings.config(
     });
     
     //construct menu to show to user
-    console.log("menu");
     resultsMenu.show();
     splashCard.hide();
     
     // Show the raw response if parsing failed
     if (e.failed) {
-      console.log(e.response);
-      console.log("failed");
+       console.log("failed to fetch JSON data");
+       var failed = new UI.Card({
+            title: "Error processing config data",
+            body: "Ensure you have proper internet connection and try restarting the app."
+       });
+       failed.show();
     }
   }
 );
